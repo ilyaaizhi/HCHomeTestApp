8e93b29eb1a5d3e4924ff920d20177ba0d696f0e
Ilya Aizhi <porolonchick@gmail.com>
2015-06-08 20:05:58 +0400

    HCHomeTestApp reads more params and throw expection in case of unknown. HTNode implementation.

--- HCHomeTestApp/src/com/ilya/hc/HCHomeTestApp.java
+++ HCHomeTestApp/src/com/ilya/hc/HCHomeTestApp.java
@@ -7,6 +7,8 @@ import com.ilya.hc.node.HTNode;
  * many nodes, but only one of them does
  * 	System.out.println("We are started!").
  * 
+ * Other nodes acknowledge user by saying "Yet another node is up."
+ * 
  * @author ilya
  *
  */
@@ -15,6 +17,10 @@ public class HCHomeTestApp {
 
 	private static String login = "admin";
 	private static String password = "AdminPassword";
+	private static String address = "127.0.0.1";
+	private static String port = "7775";
+	private static boolean dieYong = false;
+	private static String name = "";
 
 	/**
 	 * Entry point
@@ -23,9 +29,13 @@ public class HCHomeTestApp {
 	 */
 	public static void main(String[] args) {
 		readParams(args);
-
-		HTNode node = new HTNode(login, password);
-		node.doTell();
+		
+		HTNode instance = new HTNode(login, password, address, name, port);
+		
+		// Simulation of instance crash right after Hazelcast instance initialization
+		if (dieYong) System.exit(1);
+		
+		instance.doTell();
 	}
 	
 	/**
@@ -34,16 +44,28 @@ public class HCHomeTestApp {
 	 * @param args
 	 */
 	private static void readParams(String[] args) {
-		for (int i = 0; i < args.length; ++i) {
-			if (args[i].equals("-login")) {
-				login = args[++i];
-			} else if (args[i].equals("-password")) {
-				password = args[++i];
-			} else {
-				System.out.println("Usage: java -jar HCHomeTestApp.jar [-login <login>] [-password <password>]");
+		try {
+			for (int i = 0; i < args.length; ++i) {
+				if (args[i].equals("-login")) {
+					login = args[++i];
+				} else if (args[i].equals("-password")) {
+					password = args[++i];
+				} else if (args[i].equals("-address")) {
+					address = args[++i];
+				} else if (args[i].equals("-port")) {
+					port = args[++i];
+				} else if (args[i].equals("-dieYong")) {
+					dieYong = true;
+				} else if (args[i].equals("-name")) {
+					name = args[++i];
+				} else {
+					throw new Exception("Invalid argumet: " + args[i]);
+				}
 			}
+		} catch (Exception e) {
+			System.err.println(e);
+			System.err.println("Usage: java -jar HCHomeTestApp.jar [-login <login>] [-password <password>] [-address <ip address>] [-port <port number>]");
 		}
-		
 	}
 
 }
--- HCHomeTestApp/src/com/ilya/hc/node/HTNode.java
+++ HCHomeTestApp/src/com/ilya/hc/node/HTNode.java
@@ -1,36 +1,87 @@
 package com.ilya.hc.node;
 
+import java.util.Map;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.InterfacesConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 public class HTNode {
 
 	private String login;
 	private String password;
-	
+	private String address;
+	private int port;
+	private String name;
+	private String hazelcastInstanceName;
+	private HazelcastInstance instance;	
+
 	/**
 	 * HTNode is Home Task Node that represents one node.
-	 * If this node is the first one it says "We are started!"
+	 * It launches Hazelcast instance.
 	 * 
-	 * @param login
-	 * @param password
+	 * @param login for Hazelcast instance
+	 * @param password for Hazelcast instance
+	 * @param address of Hazelcast instance
+	 * @param port of Hazelcast instance
+	 * @name name of the node
 	 */
-	public HTNode(String login, String password) {
+	public HTNode(String login, String password, String address, String nodeName, String port) {
 		this.login = login;
 		this.password = password;
+		this.address = address;
+		this.name = nodeName;
+		this.port = Integer.parseInt(port);
+		this.hazelcastInstanceName = "HomeTest";
+		launchHazelcastInstance();
 	}
 
 	/**
-	 * Does System.out.println("We are started!"); if this node it the first one
-	 * Otherwise just indicates that it has been started.
+	 * Does System.out.println("We are started!"); if this node is the first one that started
+	 * Otherwise just indicates that it is started
+	 * 
 	 */
 	public void doTell() {
-		if (amITheFirst()) {
-			System.out.println("We are started!");			
+		Map<Integer, String> lockMap = instance.getMap("lockMap");
+		
+		//There is no need to lock key since put is thread safe and only the first guy will get null if key is absent
+		//If value itself would be important, it would make sense to lock
+		String value = lockMap.put(0, "Initialized");
+
+		if (value == null) {
+			//in three lines because it's explicitly said "System.out.println("We are started!")" and few \n to make it more visible
+			System.out.println("\n\n");
+			System.out.println("We are started!");
+			System.out.println("\n\n");
+			System.out.println(name);
 		} else {
-			System.out.println("Yet another node is up.");
+			//this node is not the first one, but it still would be interesting to know that node is started
+			System.out.println("Yet another node is up. " + name);
 		}
 	}
 	
-	private boolean amITheFirst() {
-		return true;
+	/**
+	 * Launches Hazelcast instance.
+	 * Uses {@link hazelcastInstanceName} {@link #address}, {@link #port}, {@link #login}, {@link #password}
+	 * 
+	 */
+	private void launchHazelcastInstance() {
+		Config config = new Config(hazelcastInstanceName);
+
+		NetworkConfig networkConfig = config.getNetworkConfig();
+		networkConfig.setPort(port);
+		
+		InterfacesConfig interfacesConfig = networkConfig.getInterfaces(); 
+		interfacesConfig.addInterface(address);
+
+		GroupConfig groupConfig = config.getGroupConfig();
+		groupConfig.setName(login);
+		groupConfig.setPassword(password);
+
+		instance = Hazelcast.newHazelcastInstance(config);	
 	}
 
 }
