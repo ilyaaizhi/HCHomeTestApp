dd8e80b5fd5d2ae4450e2da6a2a82df97c546040
Ilya Aizhi <porolonchick@gmail.com>
2015-06-08 21:52:01 +0400

    HTNode fires up just one Hazelcast node to lower memory footprint and start time

--- HCHomeTestApp/src/com/ilya/hc/node/HTNode.java
+++ HCHomeTestApp/src/com/ilya/hc/node/HTNode.java
@@ -1,7 +1,11 @@
 package com.ilya.hc.node;
 
+import java.net.Socket;
 import java.util.Map;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.GroupConfig;
 import com.hazelcast.config.InterfacesConfig;
@@ -21,7 +25,9 @@ public class HTNode {
 
 	/**
 	 * HTNode is Home Task Node that represents one node.
-	 * It launches Hazelcast instance.
+	 * It connects to existing Hazelcast instance if there is any.
+	 * Otherwise it launches new local instance.
+	 * It does not launch new instance by default to lower memory footprint.
 	 * 
 	 * @param login for Hazelcast instance
 	 * @param password for Hazelcast instance
@@ -36,7 +42,7 @@ public class HTNode {
 		this.name = nodeName;
 		this.port = Integer.parseInt(port);
 		this.hazelcastInstanceName = "HomeTest";
-		launchHazelcastInstance();
+		getHazelcastInstance();
 	}
 
 	/**
@@ -47,9 +53,9 @@ public class HTNode {
 	public void doTell() {
 		Map<Integer, String> lockMap = instance.getMap("lockMap");
 		
+		String value = lockMap.put(0, "Initialized");
 		//There is no need to lock key since put is thread safe and only the first guy will get null if key is absent
 		//If value itself would be important, it would make sense to lock
-		String value = lockMap.put(0, "Initialized");
 
 		if (value == null) {
 			//in three lines because it's explicitly said "System.out.println("We are started!")" and few \n to make it more visible
@@ -62,26 +68,68 @@ public class HTNode {
 			System.out.println("Yet another node is up. " + name);
 		}
 	}
-	
+
 	/**
-	 * Launches Hazelcast instance.
-	 * Uses {@link hazelcastInstanceName} {@link #address}, {@link #port}, {@link #login}, {@link #password}
+	 * Initialize {@link #instance}
+	 * Tries to connect to existing Hazelcast instance if {@link #address}:{@link #port} is busy
+	 * Otherwise start new local Hazelcast node
 	 * 
 	 */
-	private void launchHazelcastInstance() {
-		Config config = new Config(hazelcastInstanceName);
+	private void getHazelcastInstance() {
+		try {
+			// checking whether port is open. It's few seconds faster than trying to join right away
+			Socket s = new Socket(address, port);
+			s.close();
+			try {
+				connectToHazelcastInstance();
+			} catch (Exception e) {
+				System.err.println("Port " + port + " is busy with something else. Can't start application.");
+			}
+		} catch (Exception e) {
+			launchHazelcastInstance(false);
+		}
+	}
 
-		NetworkConfig networkConfig = config.getNetworkConfig();
-		networkConfig.setPort(port);
-		
-		InterfacesConfig interfacesConfig = networkConfig.getInterfaces(); 
-		interfacesConfig.addInterface(address);
+	/**
+	 * Connects to Hazelcast Instance and @throws IllegalStateException in case it can't find it  
+	 * Uses {@link #address}, {@link #port}, {@link #login}, {@link #password}
+	 * 
+	 */
+	private void connectToHazelcastInstance() throws IllegalStateException {
+		ClientConfig config = new ClientConfig();
+
+		ClientNetworkConfig networkConfig = config.getNetworkConfig();
+		networkConfig.addAddress(address + ":" + port);
 
 		GroupConfig groupConfig = config.getGroupConfig();
 		groupConfig.setName(login);
 		groupConfig.setPassword(password);
 
-		instance = Hazelcast.newHazelcastInstance(config);	
+		instance = HazelcastClient.newHazelcastClient(config);		
+	}
+
+	/**
+	 * Launches Hazelcast instance.
+	 * Uses {@link #address}, {@link #port}, {@link #login}, {@link #password}
+	 * 
+	 * @param portAutoIncrement allow to auto-increment port if {@link #port} is busy  
+	 */
+	private void launchHazelcastInstance(boolean portAutoIncrement) {
+		Config config = new Config(hazelcastInstanceName);
+
+		NetworkConfig networkConfig = config.getNetworkConfig();
+		
+		InterfacesConfig interfacesConfig = networkConfig.getInterfaces(); 
+		interfacesConfig.addInterface(address);
+		
+		networkConfig.setPort(port);
+		networkConfig.setPortAutoIncrement(portAutoIncrement);
+
+		GroupConfig groupConfig = config.getGroupConfig();
+		groupConfig.setName(login);
+		groupConfig.setPassword(password);
+
+		instance = Hazelcast.newHazelcastInstance(config);
 	}
 
 }
