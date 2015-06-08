891806f913418087b62a7e5d08a30d15a6b212f9
Ilya Aizhi <porolonchick@gmail.com>
2015-06-08 17:32:09 +0400

    read args HTNode

--- HCHomeTestApp/src/com/ilya/hc/HCHomeTestApp.java
+++ HCHomeTestApp/src/com/ilya/hc/HCHomeTestApp.java
@@ -1,5 +1,7 @@
 package com.ilya.hc;
 
+import com.ilya.hc.node.HTNode;
+
 /**
  * HCHomeTestApp is Hazelcast Home Test Application that can be launched in 
  * many nodes, but only one of them does
@@ -11,8 +13,31 @@ package com.ilya.hc;
 
 public class HCHomeTestApp {
 
+	private static String login = "admin";
+	private static String password = "AdminPassword";
+
 	public static void main(String[] args) {
-		System.out.println("We are started!");
+		readParams(args);
+
+		HTNode node = new HTNode(login, password); 
+	}
+	
+	/**
+	 * Read application arguments
+	 * 
+	 * @param args
+	 */
+	private static void readParams(String[] args) {
+		for (int i = 0; i < args.length; ++i) {
+			if (args[i].equals("-login")) {
+				login = args[++i];
+			} else if (args[i].equals("-password")) {
+				password = args[++i];
+			} else {
+				System.out.println("Usage: java -jar HCHomeTestApp.jar [-login <login>] [-password <password>]");
+			}
+		}
+		
 	}
 
 }
---
+++ HCHomeTestApp/src/com/ilya/hc/node/HTNode.java
@@ -0,0 +1,15 @@
+package com.ilya.hc.node;
+
+public class HTNode {
+
+	private String login;
+	private String password;
+	
+	public HTNode(String login, String password) {
+		this.login = login;
+		this.password = password;
+		
+		System.out.println("We are started!");		
+	}
+
+}
