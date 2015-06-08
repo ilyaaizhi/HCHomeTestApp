cdf6b5e10f408cccbec4742681b45d5e319e7a3d
Ilya Aizhi <porolonchick@gmail.com>
2015-06-08 17:22:08 +0400

    Initial commit

---
+++ HCHomeTestApp/src/com/ilya/hc/HCHomeTestApp.java
@@ -0,0 +1,18 @@
+package com.ilya.hc;
+
+/**
+ * HCHomeTestApp is Hazelcast Home Test Application that can be launched in 
+ * many nodes, but only one of them does
+ * 	System.out.println("We are started!").
+ * 
+ * @author ilya
+ *
+ */
+
+public class HCHomeTestApp {
+
+	public static void main(String[] args) {
+		System.out.println("We are started!");
+	}
+
+}
